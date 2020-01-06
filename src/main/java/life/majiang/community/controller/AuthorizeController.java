ad5a744ea3f1a20ed694a367cae0fc853be130db
wgdblack <630175180@qq.com>
2020-01-06 15:56:12 +0800

    code to properties20200106

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -1,8 +1,11 @@
 package life.majiang.community.controller;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +15,28 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecert;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+    //通过value的注解获得在application.properties封装的药获取的变量，然后再下面的callback使用，直接抽离了在代码里面的配置文件
+    //好处：在不同的环境可以修改成不同的配置，方便我们部署 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("014c0c337afea9483a43");
-        accessTokenDTO.setClient_secret("7ae03e74f43ec57b69bdbe1f02fe3bee37d140c0");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecert);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        githubProvider.getAccessToken(accessTokenDTO); //快捷键ctrl+alt+v出现这一步:AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);//快捷键ctrl+alt+v出现这一步:AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        System.out.println(user.getBio());
+        System.out.println(user.getId());
         return "index";
 
     }
--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -1,7 +1,9 @@
 package life.majiang.community.provider;
 //这个包就是借用别人的代码
 
+import com.alibaba.fastjson.JSON;
 import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 import sun.awt.geom.AreaOp;
@@ -12,20 +14,36 @@ import java.io.IOException;
 //如果参数超过两个，就要封装成对象
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
-         MediaType JSON = MediaType.get("application/json; charset=utf-8");
+         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
             try (Response response = client.newCall(request).execute()) {
                 String string = response.body().string();
-                System.out.println(string);//快捷键sout
-                return response.body().string();
-            } catch (IOException e) {
-
+                String token = string.split("&")[0].split("=")[1];//快捷键ctrl+alt+n
+                return token;
+                //快捷键sout输出；
+            } catch (Exception e) {
+            e.printStackTrace();
             }
             return null;
+    }
+    public GithubUser getUser(String accessToken){
+            OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+ accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);//把string的JSON对象自动转换解析成JAVA的类对象
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
     }
 }
